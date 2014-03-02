dae9d1e435b93f6757c54b9f94677039c04f992a
Mustapha <mustapha.hamoui@gmail.com>
2014-03-02 13:46:16 +0000

    fixed ipad zoom in landscape mode

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 <link rel="shortcut icon" href="http://beirutspring.com/favicon.ico?v=001" />
 <title><?php bloginfo('name'); ?> | <?php if( is_home() ) : echo bloginfo( 'description' ); endif; ?><?php wp_title( '', true ); ?></title>
 
